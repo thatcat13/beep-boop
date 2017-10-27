e018d8c7e9bb8787fce8fe92d2e8a79ea52c09c7
thatcat13 <thatcat13@gmail.com>
2017-10-27 11:44:21 -0700

    add return values to get everything to work properly with backend segregated from front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 //backend logic:
-var backendFunction = function(numArray) {
+var backendFunction = function(anyNumber) {
   var numArray = [];
-  for (var i = 0; i <=numArray; i++) {
-    numArray.push(i);
-  } //numArray for-loop
+    for (var i = 0; i <=anyNumber; i++) {
+      numArray.push(i);
+    } //numArray for-loop
 
-  for (var iNumArray = 0; iNumArray < numArray.length; iNumArray ++) {
-    if (numArray[iNumArray] === 0) {
-      numArray[iNumArray] = "Beep!"
-    } else if (numArray[iNumArray] === 1) {
-      numArray[iNumArray] = "Boop!"
-    } else if ((numArray[iNumArray]) % 3 === 0) {
-      numArray[iNumArray] = "I'm sorry, Dave. I'm afraid I can't do that."
-    }
-  } //iNum for-loop
-} //backendFunction
+    for (var iNumArray = 0; iNumArray < numArray.length; iNumArray ++) {
+      if (numArray[iNumArray] === 0) {
+        numArray[iNumArray] = "Beep!"
+      } else if (numArray[iNumArray] === 1) {
+        numArray[iNumArray] = "Boop!"
+      } else if ((numArray[iNumArray]) % 3 === 0) {
+        numArray[iNumArray] = "I'm sorry, Dave. I'm afraid I can't do that."
+      }
+    } //iNum for-loop
+    return numArray;
+  } //backendFunction
+
 
 //frontend logic
 $(document).ready(function(){
@@ -22,7 +24,6 @@ $(document).ready(function(){
     event.preventDefault();
     var userInput = parseInt($("#inputNum").val());
     var result = backendFunction(userInput);
-
-    console.log(result);
+    $("#outputText").text(result);
   }); //event
 }); //doc ready
