10ff73c4cffac1d231285b03acd99b3933c78be2
thatcat13 <thatcat13@gmail.com>
2017-10-27 11:01:31 -0700

    check backend logic using jsfiddle and confirm it works but since moving backend logic to the back there's an undefined error that needs fixing

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
 //backend logic:
+var backendFunction = function(numArray) {
+  var numArray = [];
+  for (var i = 0; i <=numArray; i++) {
+    numArray.push(i);
+  } //numArray for-loop
 
+  for (var iNumArray = 0; iNumArray < numArray.length; iNumArray ++) {
+    if (numArray[iNumArray] === 0) {
+      numArray[iNumArray] = "Beep!"
+    } else if (numArray[iNumArray] === 1) {
+      numArray[iNumArray] = "Boop!"
+    } else if ((numArray[iNumArray]) % 3 === 0) {
+      numArray[iNumArray] = "I'm sorry, Dave. I'm afraid I can't do that."
+    }
+  } //iNum for-loop
+} //backendFunction
+
+//frontend logic
 $(document).ready(function(){
   $("#beep-boop").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#inputNum").val());
-    // var numberGame = function(userInput) {
-      var numToArray = function(userInput) {
-        var numArray = [0];
-          for (var i = 0; i <=userInput; i++) {
-            numArray.push(i);
-            console.log(numArray);
-        // for (var iNumArray = 0; iNumArray < numArray.length; iNumArray ++) {
-        //   if (numArray[iNumArray] === 0) {
-        //     numArray[iNumArray] = "Beep!"
-        //   } else if (numArray[iNumArray] === 1) {
-        //     numArray[iNumArray] = "Boop!"
-        //   } else if ((numArray[iNumArray]) % 3 === 0) {
-        //     numArray[iNumArray] = "I'm sorry, Dave. I'm afraid I can't do that."
-        //   }
-        // } //iNum for-loop
-      } //numArray for-loop
-  } //numToArray
-// } //numberGame
+    var result = backendFunction(userInput);
 
-//frontend logic:
-
-    // var result = numberGame(userInput);
-
-
-
+    console.log(result);
   }); //event
 }); //doc ready
