a9d611c7d1db279f511859fa91f14e34d3ecf95e
thatcat13 <thatcat13@gmail.com>
2017-10-27 09:23:06 -0700

    begin to flesh out logic for creating number array from input number

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var numberGame = function(num) {
+  var inputNum = parseInt($("input#inputNum").val());
+
+  var numToArray = function(inputNum) {
+    var numArray = [];
+      for (var i = 0; i <=inputNum); i++) {
+        numArray.push(i);
+
+  }
+
+
+}
