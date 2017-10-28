20bc504b09f6e0148ab21ce44fcec9a9e1fae58e
thatcat13 <thatcat13@gmail.com>
2017-10-27 17:10:26 -0700

    try to start breaking down backend logic to handle different pieces

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 //backend logic:
 var backendFunction = function(anyNumber) {
-  var numArray = [];
-    for (var i = 0; i <=anyNumber; i++) {
-      if (i.length >= 2) {
-        i.
-      }
-      numArray.push(i);
+  var singleFunction = function(singleNumber) {
+    if (singleNumber.length === 1) {
+    var singlenumArray = [];
+      for (var i = 0; i <=anyNumber; i++) {
+        numArray.push(i);
+    }
 
-    } //numArray for-loop
+//this is to convert single digits into an array:
+    var number = 9;
+    var singleNumArray = [];
+    for (var i = 0; i <=number; i++) {
+      singleNumArray.push(i);
+    }
+    console.log(singleNumArray);
+
+
+
+
+
+  } //numArray for-loop
     for (var iNumArray = 0; iNumArray < numArray.length; iNumArray ++) {
 
       if (numArray.includes(0)) {
@@ -22,7 +34,7 @@ var backendFunction = function(anyNumber) {
     } //iNum for-loop
 
     return numArray;
-  } //backendFunction
+} //backendFunction
 
 
 
