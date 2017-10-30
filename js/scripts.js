bf4d0fdcf83d374985731deb17ec2bbf542fde42
thatcat13 <thatcat13@gmail.com>
2017-10-29 17:11:33 -0700

    logic to start with number array, check %3 ===0, then turn into string to compare 1s and 0s in string format

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,27 @@
 //backend logic:
-var backendFunction = function(anyNumber) {
-  var singleFunction = function(singleNumber) {
-    if (singleNumber.length === 1) {
-    var singlenumArray = [];
-      for (var i = 0; i <=anyNumber; i++) {
-        numArray.push(i);
+var beepBoop = function(anyNumber) {
+  var numArray = [];
+    for (var i = 0; i <= anyNumber; i++) {
+      numArray.push(i);
     }
 
-//this is to convert single digits into an array:
-    var number = 9;
-    var singleNumArray = [];
-    for (var i = 0; i <=number; i++) {
-      singleNumArray.push(i);
-    }
-    console.log(singleNumArray);
-
-
-
-
-
-  } //numArray for-loop
     for (var iNumArray = 0; iNumArray < numArray.length; iNumArray ++) {
+      if (numArray[iNumArray] % 3 === 0) {
+        numArray[iNumArray] = "I'm sorry, Dave. I'm afraid I can't do that."
+      }
+    }
+    var stringArray = numArray.map(function(x) {
+      return x.toString();
+    });
 
-      if (numArray.includes(0)) {
-        numArray[iNumArray] = "Beep!"
-      } else if (numArray.includes(1)) {
+    for (var iStrArray = 0; iStrArray < stringArray.length; iStrArray ++) {
+      if (stringArray[iStrArray] = "0") {
+        numArray[iStrArray] = "Beep!"
+      } else if (stringArray[iStrArray] = "1") {
         numArray[iNumArray] = "Boop!"
       }
-
-      // if ((numArray[iNumArray]).includes(%3 === 0)) {
-      //   numArray[iNumArray] = "I'm sorry, Dave. I'm afraid I can't do that."
-
-    } //iNum for-loop
-
-    return numArray;
+    }
+    return stringArray;
 } //backendFunction
 
 
@@ -44,7 +32,7 @@ $(document).ready(function(){
     event.preventDefault();
     var userInput = parseInt($("#inputNum").val());
     $("#returnList").empty();
-    var result = backendFunction(userInput);
+    var result = beepBoop(userInput);
 
     result.forEach(function(li) {
       $("#returnList").append("<li>" + li + "</li>")
