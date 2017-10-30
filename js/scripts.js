6d871fb776329323354e4d15b98751f4eeb89a29
thatcat13 <thatcat13@gmail.com>
2017-10-29 20:01:40 -0700

    continue to work with business logic and successfully create array with numbers that are replaced with dave, and after that an array of strings with a mixture of remaining numbers and the string dave throughout

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,43 @@
 //backend logic:
 var beepBoop = function(anyNumber) {
   var numArray = [];
-    for (var i = 0; i <= anyNumber; i++) {
+    for (var i = 0; i <=anyNumber; i++) {
       numArray.push(i);
     }
 
-    for (var iNumArray = 0; iNumArray < numArray.length; iNumArray ++) {
+    for (var iNumArray = 1; iNumArray < numArray.length; iNumArray ++) {
       if (numArray[iNumArray] % 3 === 0) {
         numArray[iNumArray] = "I'm sorry, Dave. I'm afraid I can't do that."
       }
     }
+
     var stringArray = numArray.map(function(x) {
       return x.toString();
     });
 
     for (var iStrArray = 0; iStrArray < stringArray.length; iStrArray ++) {
-      if (stringArray[iStrArray] = "0") {
-        numArray[iStrArray] = "Beep!"
-      } else if (stringArray[iStrArray] = "1") {
-        numArray[iNumArray] = "Boop!"
+      if (stringArray[iStrArray].includes("0")) {
+        return stringArray[iStrArray] = "Beep!"
+      } else if (stringArray[iStrArray].includes("1")) {
+        return stringArray[iStrArray] = "Boop!"
       }
     }
-    return stringArray;
+    console.log(stringArray);
 } //backendFunction
 
 
 
-//frontend logic
-$(document).ready(function(){
-  $("#beep-boop").submit(function(event) {
-    event.preventDefault();
-    var userInput = parseInt($("#inputNum").val());
-    $("#returnList").empty();
-    var result = beepBoop(userInput);
-
-    result.forEach(function(li) {
-      $("#returnList").append("<li>" + li + "</li>")
-      $("form")[0].reset();
-    });
-  }); //event
-}); //doc ready
+// //frontend logic
+// $(document).ready(function(){
+//   $("#beep-boop").submit(function(event) {
+//     event.preventDefault();
+//     var userInput = parseInt($("#inputNum").val());
+//     $("#returnList").empty();
+//     var result = beepBoop(userInput);
+//
+//     result.forEach(function(li) {
+//       $("#returnList").append("<li>" + li + "</li>")
+//       $("form")[0].reset();
+//     });
+//   }); //event
+// }); //doc ready
