8adc5b261a27dd2679602ac40f90373c887c8241
thatcat13 <thatcat13@gmail.com>
2017-10-30 17:17:37 -0700

    try to correct syntax for beep boop for-loop but broke it

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,30 @@ var beepBoop = function(anyNumber) {
       numArray.push(i);
     }
 
-    for (var iNumArray = 1; iNumArray < numArray.length; iNumArray ++) {
-      if (numArray[iNumArray] % 3 === 0) {
-        numArray[iNumArray] = "I'm sorry, Dave. I'm afraid I can't do that."
+    for (var i = 1; i < numArray.length; i++) {
+      if (numArray[i] % 3 === 0) {
+        numArray[i] = "I'm sorry, Dave. I'm afraid I can't do that."
       }
     }
 
     var stringArray = numArray.map(function(x) {
       return x.toString();
+
     });
 
-    for (var iStrArray = 0; iStrArray < stringArray.length; iStrArray ++) {
-      if (stringArray[iStrArray].includes["0"]) {
-        return stringArray[iStrArray] === "Beep!";
-      } else if (stringArray[iStrArray].includes["1"]) {
-        return stringArray[iStrArray] === "Boop!";
+    for (var i = 0; i < stringArray.length; i++) {
+      if (stringArray[i].includes("0") {
+        stringArray[i] = "Beep!";
+      } else if (stringArray[i].includes("1") {
+        stringArray[i] = "Boop!";
       }
     }
-    console.log(stringArray);
+    return stringArray;
 } //backendFunction
 
 
 
-//frontend logic
+// frontend logic
 $(document).ready(function(){
   $("#beep-boop").submit(function(event) {
     event.preventDefault();
